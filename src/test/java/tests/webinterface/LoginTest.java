0fb78102110a19a9ced726c4f238804a70a5f338
mikhail <ishagunmv@gmail.com>
2022-05-06 14:26:36 +0300

    FIX: remove unused lib

--- src/test/java/tests/webinterface/LoginTest.java
+++ src/test/java/tests/webinterface/LoginTest.java
@@ -1,11 +1,8 @@
 package tests.webinterface;
 
-import data.EUsers;
-import org.junit.jupiter.api.Test;
-import ssh.DeviceConnect;
 import tests.base.BaseTest;
 
-import static common.Config.DEVICE_IP;
 
 public class LoginTest extends BaseTest {
+
 }
